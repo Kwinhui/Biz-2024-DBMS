3dff9e0d1c02c1982235bb2cd72288ec103299d2
n96js <n96js@naver.com>
2024-01-10 13:39:15 +0900

    DBMS

---
+++ root(2024-01-09-start).sql
@@ -0,0 +1,17 @@
+/*
+ 여러줄 주석문 
+ */
+-- 한줄짜리 주석
+
+/* 
+ 이 화면에서 SQL 명령어를 입력하고
+ Ctrl + Enter 를 입력하면 명령이 실행된다.
+ 명령어는 대체로 대소문자 구분이 없다
+ 중요 키워드는 대문자로, 변수 등은 소문자로 입력한다.
+ */
+SHOW DATABASES;
+SHOW DATABASES;
+USE mysql;	-- mysql 스키마에 대해 명령어 실행
+SHOW TABLES; -- mysql 스키마에 대한 테이블들을 보여달라
+SELECT * FROM user;	-- 그중에서 user table으로 부터 *(전체)를 선택해서 보여줘
+ 
